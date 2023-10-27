3499f70de6efcc64d2e1d7d96749e5a6f87984fa
Tewodros01 <tewodrosm01@gmail.com>
2023-10-27 10:12:18 +0300

    DB COnfigerd

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,5 +1,5 @@
 const MONGO_DB_CONFIG = {
-  DB: "mongodb+srv://Tewodros:996633@cluster0.4dymzm9.mongodb.net/?retryWrites=true&w=majority",
+  DB: "mongodb+srv://tewodrosm01:996633@cluster0.gcyapaq.mongodb.net/?retryWrites=true&w=majority",
   PAGE_SIZE: 10,
   // BCRYPT_PASSWORD: "Grocery App",
   SALT_ROUND: 10,
