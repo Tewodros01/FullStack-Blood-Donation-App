3990465fcbfb5f60d49e763096fca74feb4b26c9
Tewodros01 <tewodrosm01@gmail.com>
2023-10-27 19:36:13 +0300

    API IsWorking

--- backend/src/config/db.connect.ts
+++ backend/src/config/db.connect.ts
@@ -1,28 +1,18 @@
 import MONGO_DB_CONFIG from "./database.js";
 import * as mongoDB from "mongodb";
 
-// const connectDB = async () => {
-//   try {
-//     mongoose.set("strictQuery", true);
-//     await mongoose.connect(MONGO_DB_CONFIG.DB);
-//     console.log("Database is connected");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 export async function connectToDatabase() {
   try {
+    // Create a new MongoDB client with the connection string from MONGO_DB_CONFIG.DB.
     const client: mongoDB.MongoClient = new mongoDB.MongoClient(
       MONGO_DB_CONFIG.DB
     );
-
+    // Connect to the MongoDB server.
     await client.connect();
-
-    console.log(`Successfully connected to database `);
+    // Connection successful, log a message.
+    console.log(`Successfully connected to the database.`);
   } catch (error) {
-    console.log(error);
+    // Handle any errors that occur during the connection attempt.
+    console.error("Error connecting to the database:", error);
   }
 }
-
-export default connectToDatabase;
